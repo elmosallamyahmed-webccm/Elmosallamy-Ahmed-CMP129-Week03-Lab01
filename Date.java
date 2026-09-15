9555b01b18f480bd279195dff4c1f38affb57375
elmosallamyahmed-webccm <elmosallamy.ahmed@student.ccm.edu>
2026-09-15 18:09:32 -0400

    Finished Part 1

--- Date.java
+++ Date.java
@@ -4,7 +4,8 @@ public class Date {
     int day;
     int month;
     int year;
-    public int getDate(int day, int month, int year){
+    final String [] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    public void getDate(){
         System.out.println("What is the day in '00' formatting?");
         this.day = input.nextInt();
         while( day < 1 || day >31){
@@ -14,10 +15,21 @@ public class Date {
         }
         System.out.println("What is the month in '00' formatting?");
         this.month = input.nextInt();
-        while( month < 1 || month >31){
+        while( month < 1 || month >12){
             System.out.println("Please ensure it is an integer greater than 0 and less than 13");
             System.out.println("What is the month in '00' formatting?");
             this.month = input.nextInt();
         }
+        System.out.println("What is the year in '0000' formatting?");
+        this.year = input.nextInt();
+    }
+    public void format1(){
+        System.out.printf("%d / %d / %d%n", month, day, year);
+    }
+    public void format2(){
+        System.out.printf("%s %d, %d%n", months[month-1], day, year);
+    }
+    public void format3(){
+        System.out.printf("%d %s %d%n", day, months[month-1], year);
     }
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,9 @@
 public class DateTest {
-    
+    public static void main(String[] args){
+        Date date1 = new Date();
+        date1.getDate();
+        date1.format1();
+        date1.format2();
+        date1.format3();
+    }
 }
